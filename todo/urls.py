2167aa4714d1b5d3620484b3f5742093f3478c68
Chase <chase7106@gmail.com>
2025-05-09 20:45:06 +0800

    跟著老師完成首頁代辦事項

--- todo/urls.py
+++ todo/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from .views import todolist
 
 urlpatterns = [
-    path("", include("todo.urls")),
-    path("admin/", admin.site.urls),
+    path("", todolist),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Todo
+
 
 # Create your views here.
+def todolist(request):
+    todos = Todo.objects.all()
+    print(todos)
+    result = {"todos": todos}
+    return render(request, "todo/todolist.html", result)
